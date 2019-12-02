395ab55f2c52051ae4bf82ba1da16dd6353dc9f0
ariana <ariana.daney@ssfs.org>
2019-12-02 09:00:26 -0500

    make a class for dog with commands

---
+++ dog.py
@@ -0,0 +1,29 @@
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.trick_list = []
+
+    def get_name(self):
+        return self.name
+        self.
+
+    def print_name(self):
+        print(self.name)
+
+    def sit(self):
+        print(self.name + " sit")
+
+    def lay(self):
+        print(self.name + " lay down")
+
+    def roll(self):
+        print(self.name + " roll over")
+
+    def catch(self):
+        print(self.name + " catch squirrels")
+
+    def soccer(self):
+        print(self.name + " go get the ball")
+
+    def print_trick_list(self):
---
+++ dog_test.py
@@ -0,0 +1,8 @@
+import dog
+
+my_dog = dog.Dog("Piper")
+my_dog.sit()
+my_dog.lay()
+my_dog.roll()
+my_dog.catch()
+my_dog.soccer()
