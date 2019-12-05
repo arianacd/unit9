124bce6a23b26464666119f151d4a8e86dbfd1ef
ariana <ariana.daney@ssfs.org>
2019-12-05 12:50:29 -0500

    unit 9 option 1 final project flowchart and started the project

---
+++ unit9option1project.py
@@ -0,0 +1,11 @@
+# by ariana daney
+# last modified december 5, 2019
+# unit 9 option 1 - this program creates a small game of war that the user can play
+
+import card
+import deck
+user_cards = []
+dealer_cards = []
+
+
+def deal_card():
