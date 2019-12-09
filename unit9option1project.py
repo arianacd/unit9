e64b77a79831fcc98e927c8cb25875880cd1210e
ariana <ariana.daney@ssfs.org>
2019-12-09 15:09:30 -0500

    worked on unit 9 project

--- unit9option1project.py
+++ unit9option1project.py
@@ -1,8 +1,8 @@
 # by ariana daney
-# last modified december 5, 2019
+# last modified december 9, 2019
 # unit 9 option 1 - this program creates a small game of war that the user can play
 
-import card
+
 import deck
 
 
@@ -19,15 +19,23 @@ def compare_cards(user_card, dealer_card):
         print("the user wins")
     elif user_card < dealer_card:
         print("the dealer wins!")
-    else:
-        print("its a tie!")
 
 
 def main():
     my_deck = deck.Deck()
     my_deck.shuffle()
     user_deal = deal_cards(my_deck)
+    print("The user has the cards:")
+    for y in user_deal:
+        print(y)
     dealer_deal = deal_cards(my_deck)
+    print("")
+    print("The dealer has the cards:")
+    for z in dealer_deal:
+        print(z)
+    print("")
+    for x in range(5):
+        compare_cards(user_deal[x], dealer_deal[x])
 
 
 main()
